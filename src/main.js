ffdde3b2e961f04c3f13aa743c8b2bc764930a1a
Daniel Schmidt <danielmschmidt92@gmail.com>
2021-07-01 15:28:33 +0200

    initial commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Greeter = void 0;
+class Greeter {
+    constructor(props) {
+        this.greetee = props.greetee;
+    }
+    greet() {
+        return `Hello, ${this.greetee}!`;
+    }
+}
+exports.Greeter = Greeter;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIm1haW4udHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O0FBSUEsTUFBYSxPQUFPO0lBR2xCLFlBQW1CLEtBQW1CO1FBQ3BDLElBQUksQ0FBQyxPQUFPLEdBQUcsS0FBSyxDQUFDLE9BQU8sQ0FBQztJQUMvQixDQUFDO0lBRU0sS0FBSztRQUNWLE9BQU8sVUFBVSxJQUFJLENBQUMsT0FBTyxHQUFHLENBQUM7SUFDbkMsQ0FBQztDQUNGO0FBVkQsMEJBVUMiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgaW50ZXJmYWNlIEdyZWV0ZXJQcm9wcyB7XG4gIHJlYWRvbmx5IGdyZWV0ZWU6IHN0cmluZztcbn1cblxuZXhwb3J0IGNsYXNzIEdyZWV0ZXIge1xuICBwcml2YXRlIHJlYWRvbmx5IGdyZWV0ZWU6IHN0cmluZztcblxuICBwdWJsaWMgY29uc3RydWN0b3IocHJvcHM6IEdyZWV0ZXJQcm9wcykge1xuICAgIHRoaXMuZ3JlZXRlZSA9IHByb3BzLmdyZWV0ZWU7XG4gIH1cblxuICBwdWJsaWMgZ3JlZXQoKTogc3RyaW5nIHtcbiAgICByZXR1cm4gYEhlbGxvLCAke3RoaXMuZ3JlZXRlZX0hYDtcbiAgfVxufVxuIl19
